26ae5bb24385833939742c23769dd7cd3115c266
Vaclav Elias <vaclavelias@gmail.com>
2025-02-05 23:35:18 +0000

    feat: Raycast logic updated

--- examples/code-only/Example15_Constraint/Program.cs
+++ examples/code-only/Example15_Constraint/Program.cs
@@ -6,10 +6,25 @@ using Stride.CommunityToolkit.Rendering.ProceduralModels;
 using Stride.CommunityToolkit.Skyboxes;
 using Stride.Core.Mathematics;
 using Stride.Engine;
+using Stride.Games;
+using Stride.Input;
+
+// Game entities and components
+CameraComponent? mainCamera = null;
+Entity? entity1 = null;
+Entity? entity2 = null;
+BodyComponent? body1 = null;
+BodyComponent? body2 = null;
+
+bool isButtonPressed = false;
+bool movementStoped = false;
 
 using var game = new Game();
 
-game.Run(start: (Scene rootScene) =>
+// Run the game loop with the Start and Update methods
+game.Run(start: Start, update: Update);
+
+void Start(Scene scene)
 {
     game.SetupBase3DScene();
     game.AddSkybox();
@@ -19,15 +34,15 @@ game.Run(start: (Scene rootScene) =>
 
     entity.Transform.Position = new Vector3(0, 8, 0);
 
-    entity.Scene = rootScene;
+    entity.Scene = scene;
 
-    var entity1 = game.Create3DPrimitive(PrimitiveModelType.Sphere);
+    entity1 = game.Create3DPrimitive(PrimitiveModelType.Sphere);
     entity1.Transform.Position = new Vector3(-2, 8, -2);
-    var body1 = entity1.Get<BodyComponent>();
+    body1 = entity1.Get<BodyComponent>();
 
-    var entity2 = game.Create3DPrimitive(PrimitiveModelType.Sphere);
+    entity2 = game.Create3DPrimitive(PrimitiveModelType.Sphere);
     entity2.Transform.Position = new Vector3(-2.1f, 16, -2.9f);
-    var body2 = entity2.Get<BodyComponent>();
+    body2 = entity2.Get<BodyComponent>();
 
     var constrain1 = new DistanceLimitConstraintComponent
     {
@@ -52,6 +67,54 @@ game.Run(start: (Scene rootScene) =>
 
     //entity1.Add(constrain2);
 
-    entity1.Scene = rootScene;
-    entity2.Scene = rootScene;
-});
+    entity1.Scene = scene;
+    entity2.Scene = scene;
+
+    // Retrieve the active camera from the scene
+    mainCamera = scene.GetCamera();
+}
+
+void Update(Scene scene, GameTime time)
+{
+    if (mainCamera == null) return;
+
+    if (game.Input.IsMouseButtonDown(MouseButton.Left))
+    {
+        isButtonPressed = true;
+    }
+
+    if (isButtonPressed && game.Input.IsMouseButtonDown(MouseButton.Left))
+    {
+        // Cast a ray from the camera into the scene based on the mouse position
+        var hit = mainCamera.Raycast(game.Input.MousePosition, 100, out var hitInfo);
+
+        if (hit)
+        {
+            if (hitInfo.Collidable.Entity == entity1)
+            {
+                if (!movementStoped)
+                {
+                    body1.LinearVelocity = Vector3.Zero;
+                    body1.AngularVelocity = Vector3.Zero;
+
+                    movementStoped = true;
+                }
+
+                body1.Position = hitInfo.Point;
+
+                Console.WriteLine($"Hit entity: {hitInfo.Point}");
+            }
+            else
+            {
+                Console.WriteLine($"No desired hit.");
+            }
+        }
+
+    }
+
+    if (isButtonPressed && game.Input.IsMouseButtonReleased(MouseButton.Left))
+    {
+        isButtonPressed = false;
+        movementStoped = false;
+    }
+}
