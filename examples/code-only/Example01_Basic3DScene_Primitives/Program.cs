57df4e2c2c61c9f7f0ed85af8a358b50aeba9d8c
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-09-13 21:07:29 +0100

    feat: Other primitives added

--- examples/code-only/Example01_Basic3DScene_Primitives/Program.cs
+++ examples/code-only/Example01_Basic3DScene_Primitives/Program.cs
@@ -5,6 +5,9 @@ using Stride.CommunityToolkit.Skyboxes;
 using Stride.Core.Mathematics;
 using Stride.Engine;
 
+var size1 = new Vector3(0.5f);
+var size2 = new Vector3(0.25f, 0.5f, 0.25f);
+
 using var game = new Game();
 
 game.Run(start: Start);
@@ -15,30 +18,40 @@ void Start(Scene rootScene)
     game.AddSkybox();
     game.AddProfiler();
 
-    var entity = game.Create3DPrimitive(PrimitiveModelType.Cube);
-    entity.Transform.Position = new Vector3(-4f, 0.5f, 0);
+    var cube = game.Create3DPrimitive(PrimitiveModelType.Cube,
+        new() { Size = size1 });
+    cube.Transform.Position = new Vector3(-4f, 0.5f, 0);
     //entity.Add(new DebugRenderComponentScript());
-    entity.Scene = rootScene;
+    cube.Scene = rootScene;
 
-    var entity5 = game.Create3DPrimitive(PrimitiveModelType.Cone, new() { Size = new(0.5f, 3, 0) });
-    //entity5.Add(new CollidableGizmoScript() { Color = new Color4(0.25f, 0, 0, 0.5f) });
-    entity5.Transform.Position = new Vector3(0, 2, 0);
-    entity5.Scene = rootScene;
+    var cuboid = game.Create3DPrimitive(PrimitiveModelType.Cube,
+    new() { Size = size2 });
+    cuboid.Transform.Position = new Vector3(-2, 0.5f, -4f);
+    cuboid.Scene = rootScene;
 
-    var entity3 = game.Create3DPrimitive(PrimitiveModelType.Capsule);
-    entity3.Transform.Position = new Vector3(0.01f, 6, 0);
-    entity3.Scene = rootScene;
+    var cone = game.Create3DPrimitive(PrimitiveModelType.Cone,
+        new() { Size = new(0.5f, 3, 0) });
+    //entity2.Add(new CollidableGizmoScript() { Color = new Color4(0.25f, 0, 0, 0.5f) });
+    cone.Transform.Position = new Vector3(0, 2, 0);
+    cone.Scene = rootScene;
 
-    var entity6 = game.Create3DPrimitive(PrimitiveModelType.Sphere);
-    entity6.Transform.Position = new Vector3(0, 8, 0);
-    entity6.Scene = rootScene;
+    var capsule = game.Create3DPrimitive(PrimitiveModelType.Capsule,
+        new() { Size = size2 });
+    capsule.Transform.Position = new Vector3(0.01f, 6, 0);
+    capsule.Scene = rootScene;
 
-    //var entity2 = game.Create3DPrimitive(PrimitiveModelType.Teapot, new()
-    //{
-    //    Material = game.CreateMaterial(Color.Blue)
-    //});
-    //entity2.Scene = rootScene;
+    var sphere = game.Create3DPrimitive(PrimitiveModelType.Sphere,
+        new() { Size = size2 });
+    sphere.Transform.Position = new Vector3(0, 8, 0);
+    sphere.Scene = rootScene;
 
+    var cylinder = game.Create3DPrimitive(PrimitiveModelType.Cylinder,
+        new() { Size = size2 });
+    cylinder.Transform.Position = new Vector3(0.5f, 10, 0);
+    cylinder.Scene = rootScene;
+
+    //var entity5 = game.Create3DPrimitive(PrimitiveModelType.Teapot);
+    //entity5.Scene = rootScene;
 
     //var entity4 = game.Create3DPrimitive(PrimitiveModelType.Torus);
     //entity4.Transform.Position = new Vector3(0, 4, 0);
