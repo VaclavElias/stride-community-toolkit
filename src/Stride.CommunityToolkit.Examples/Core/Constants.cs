eec02beb5c3fdf45acc14933855802b13c598843
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-09-13 20:32:36 +0100

    feat: Examples Launcher UI improvements

--- src/Stride.CommunityToolkit.Examples/Core/Constants.cs
+++ src/Stride.CommunityToolkit.Examples/Core/Constants.cs
@@ -4,4 +4,7 @@ public static class Constants
 {
     public const string Clear = "Clear";
     public const string Quit = "Quit";
+    public const string BasicExample = "1 - Basic Example";
+    public const string AdvanceExample = "2 - Advance Example";
+    public const string OtherExample = "3 - Other Example";
 }
