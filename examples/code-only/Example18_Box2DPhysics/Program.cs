14eca897ec21ecb409c30caf7f4896c717e249f2
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2026-08-31 03:41:41 +0100

    refactor: namespace updates

--- examples/code-only/Example18_Box2DPhysics/Program.cs
+++ examples/code-only/Example18_Box2DPhysics/Program.cs
@@ -1,6 +1,6 @@
 using Example18_Box2DPhysics;
-using Stride.CommunityToolkit.Box2D;
 using Example18_Box2DPhysics.Helpers;
+using Stride.CommunityToolkit.Box2D;
 using Stride.CommunityToolkit.Engine;
 using Stride.Core.Mathematics;
 using Stride.Engine;
